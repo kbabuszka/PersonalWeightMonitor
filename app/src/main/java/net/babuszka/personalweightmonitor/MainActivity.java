1636bd447193e51a1cf3a3604b67ec52d7f3f4a6
Kamil <kamil@osp.slawa.pl>
2019-01-01 17:59:41 +0100

    Add icons to the bottom menu

--- app/src/main/java/net/babuszka/personalweightmonitor/MainActivity.java
+++ app/src/main/java/net/babuszka/personalweightmonitor/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loadFragment(new DashboardFragment());
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
