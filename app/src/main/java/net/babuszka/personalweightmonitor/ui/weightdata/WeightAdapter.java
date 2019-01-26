606e7ed10ca5b38ccc4f5dff0d96ce0503a040e8
Kamil <kamil@osp.slawa.pl>
2019-01-27 00:38:59 +0100

    Implement weight change value in View Data fragment

--- app/src/main/java/net/babuszka/personalweightmonitor/ui/weightdata/WeightAdapter.java
+++ app/src/main/java/net/babuszka/personalweightmonitor/ui/weightdata/WeightAdapter.java
@@ -1,10 +1,13 @@
 package net.babuszka.personalweightmonitor.ui.weightdata;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.babuszka.personalweightmonitor.R;
@@ -12,6 +15,8 @@ import net.babuszka.personalweightmonitor.data.model.Weight;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import android.content.Context;
 
 public class WeightAdapter extends RecyclerView.Adapter<WeightAdapter.WeightViewHolder> {
 
@@ -28,8 +33,36 @@ public class WeightAdapter extends RecyclerView.Adapter<WeightAdapter.WeightView
     @Override
     public void onBindViewHolder(@NonNull WeightViewHolder weightViewHolder, int i) {
         Weight weightItem = weightList.get(i);
-        weightViewHolder.tvWeight.setText((weightItem.getWeight() == null) ? "" : weightItem.getWeight().toString());
+        weightViewHolder.tvWeight.setText((weightItem.getWeight() == null) ? "" : weightItem.getWeight().toString() + " kg");
         weightViewHolder.tvDate.setText(weightItem.getDate().toString());
+
+        if(i<weightList.size()-1) {
+            Weight nextWeightItem = weightList.get(i+1);
+            if(nextWeightItem != null) {
+                Double change = weightItem.getWeight() - nextWeightItem.getWeight();
+                weightViewHolder.tvWeightChange.setText((nextWeightItem != null) ? String.format("%.1f", change) : "");
+
+                if(change > 0) {
+                    weightViewHolder.tvWeightChange.setTextColor(Color.parseColor("#FF0000"));
+                    weightViewHolder.imageChangeArrow.setImageResource(R.drawable.ic_arrow_red_24dp);
+
+                }
+
+                if(change < 0) {
+                    weightViewHolder.tvWeightChange.setTextColor(Color.parseColor("#32a000"));
+                    weightViewHolder.imageChangeArrow.setImageResource(R.drawable.ic_arrow_green_24dp);
+                    weightViewHolder.imageChangeArrow.setRotation((float) 90.0);
+                }
+
+                if(change == 0) {
+                    weightViewHolder.tvWeightChange.setTextColor(Color.parseColor("#000000"));
+                    weightViewHolder.imageChangeArrow.setVisibility(View.INVISIBLE);
+                }
+            }
+        } else {
+            weightViewHolder.tvWeightChange.setText("");
+            weightViewHolder.imageChangeArrow.setVisibility(View.INVISIBLE);
+        }
     }
 
     @Override
@@ -49,11 +82,15 @@ public class WeightAdapter extends RecyclerView.Adapter<WeightAdapter.WeightView
     class WeightViewHolder extends RecyclerView.ViewHolder {
         private TextView tvWeight;
         private TextView tvDate;
+        private TextView tvWeightChange;
+        private ImageView imageChangeArrow;
 
         public WeightViewHolder(View itemView) {
             super(itemView);
             tvWeight = itemView.findViewById(R.id.text_weight);
             tvDate = itemView.findViewById(R.id.text_date);
+            tvWeightChange = itemView.findViewById(R.id.text_weight_change);
+            imageChangeArrow = itemView.findViewById(R.id.imageview_arrow_change);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
